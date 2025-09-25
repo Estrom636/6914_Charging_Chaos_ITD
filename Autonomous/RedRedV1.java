d3dcfb1c133549794a0291446ad83a0a7ba3ad01
Estrom636 <emmittesselstrom@gmail.com>
2025-09-25 14:48:14 -0500

    RedRedV1.java commented

--- Autonomous/RedRedV1.java
+++ Autonomous/RedRedV1.java
@@ -22,31 +22,41 @@ public class RedRedV1 extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         Hardware drive = new Hardware(hardwareMap, new Pose2d(-8.25, 64, Math.PI/2));
 
+        //Setting Pose Storage Variables
         PoseStorage.aliCol = true;
         PoseStorage.scoreType = true;
 
+        //Setting intake inizalization position
         drive.intakeLift.setPosition(0.8);
         drive.intakeLift2.setPosition(0.8);
         drive.horizontal.setPosition(0.6);
         drive.horizontal2.setPosition(0.8);
 
+        //Setting lift arm inizalization position
         drive.liftLeftS.setPosition(0.6);
         drive.liftRightS.setPosition(1);
 
+        //Setting claw inizalization position
         drive.claw.setPosition(.8);
 
+        //setting the LED colors
+        //red and off
         drive.lightRight.setPosition(0.3);
         drive.lightLeft.setPosition(0.3);
         drive.intakeLight.setPosition(0);
 
+        //creating the fail safe variable
         boolean pickUpFail;
 
+        //Create and starting limelight
         drive.limelight.start();
         drive.limelight.pipelineSwitch(1);
         drive.limelight.deleteSnapshots();
         LLResult result;
         drive.limelight.start();
 
+        //Checking if limelight is reading data
+        //This is check by just holding a sample infront of the camera
         while(!isStarted()){
             result = drive.limelight.getLatestResult();
             if (result != null && result.isValid()) {
@@ -59,9 +69,11 @@ public class RedRedV1 extends LinearOpMode {
         }
 
 
+        //reset the lift motor encoders
         drive.liftRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         drive.liftLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        //this is to wait till the start button on the driver station is pressed
         waitForStart();
 
         drive.liftRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -70,6 +82,7 @@ public class RedRedV1 extends LinearOpMode {
 
 
         //PreLoad
+        //move the lift up to a set height and then hold
         while (((drive.liftLeft.getCurrentPosition() * -1) < 1000) && drive.liftRight.getCurrentPosition() < 1000){
             drive.liftRight.setPower(Math.max((1000-(drive.liftRight.getCurrentPosition())) * 0.005, 0.1));
             drive.liftLeft.setPower(Math.min((1000-(drive.liftLeft.getCurrentPosition() * -1)) * -0.005, -0.1));
@@ -77,9 +90,12 @@ public class RedRedV1 extends LinearOpMode {
         drive.liftRight.setPower(.001);
         drive.liftLeft.setPower(-.001);
 
+        //put the lift arm into score position
         drive.liftLeftS.setPosition(0.20);
         drive.liftRightS.setPosition(0.35);
 
+        //move to and score on the high chamber by slaming into it
+        //update the position in poseition storage
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(-8.25,64,Math.PI/2))
                         .strafeToSplineHeading(new Vector2d(-4, 30), Math.PI/2)
@@ -88,9 +104,11 @@ public class RedRedV1 extends LinearOpMode {
         PoseStorage.currentPose = drive.pose;
 
 
+        //lift intake
         drive.intakeLift.setPosition(0.48);
         drive.intakeLift2.setPosition(0.48);
 
+        //lower the lift away from the high chamber and the hold
         while (((drive.liftLeft.getCurrentPosition() * -1) > 800) && drive.liftRight.getCurrentPosition() > 800){
             drive.liftRight.setPower(-1);
             drive.liftLeft.setPower(1);
@@ -98,9 +116,12 @@ public class RedRedV1 extends LinearOpMode {
         drive.liftRight.setPower(.001);
         drive.liftLeft.setPower(-.001);
 
+        //open claw
         drive.claw.setPosition(.45);
 
 
+        //move around the submersible, then push the smaple on the inner spike mark into observation zone
+        //update the position in poseition storage
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(-4,30,Math.PI/2))
                         .waitSeconds(0.125)
@@ -113,6 +134,7 @@ public class RedRedV1 extends LinearOpMode {
                         .build()
         );
         PoseStorage.currentPose = drive.pose;
+        //lower lift all the way down
         while (!drive.rightT.isPressed() && !drive.leftT.isPressed()){
             drive.liftRight.setPower(-1);
             drive.liftLeft.setPower(1);
@@ -120,17 +142,23 @@ public class RedRedV1 extends LinearOpMode {
 
 
         //Specimen 2
+        //reset the lift encoders
         drive.liftRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         drive.liftLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        //open claw
         drive.claw.setPosition(.45);
 
         drive.liftRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         drive.liftLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        //set lift arm to wall pickup location
         drive.liftLeftS.setPosition(0.25);
         drive.liftRightS.setPosition(0.65);
 
+        //check if the rear discance sensors are reading data
+        //if no -> set pickUpFail to true and turn the LEDs white
+        //if yes -> set pickUpFail to false and turn the LEDs red
         if(drive.rBack.getDistance(DistanceUnit.INCH) == 0 || drive.lBack.getDistance(DistanceUnit.INCH) == 0){
             pickUpFail = true;
             drive.lightRight.setPosition(1);
@@ -141,7 +169,10 @@ public class RedRedV1 extends LinearOpMode {
             drive.lightLeft.setPosition(0.3);
         }
 
+        //back up well the 2 rear distance sensor average distance is greater the 6.5
+        //after set movement to zero and update the position in poseition storage
         while((drive.rBack.getDistance(DistanceUnit.INCH) + drive.lBack.getDistance(DistanceUnit.INCH)) / 2 >= 6.5){
+            //do same sensor data check as above
             if(drive.rBack.getDistance(DistanceUnit.INCH) == 0 || drive.lBack.getDistance(DistanceUnit.INCH) == 0){
                 pickUpFail = true;
                 drive.lightRight.setPosition(1);
@@ -151,20 +182,25 @@ public class RedRedV1 extends LinearOpMode {
                 drive.lightRight.setPosition(0.3);
                 drive.lightLeft.setPosition(0.3);
             }
+            //set the movement to 0.25 backwards and update drive position
             drive.setDrivePowers(new PoseVelocity2d(new Vector2d(-0.25, 0), 0));
             drive.updatePoseEstimate();
         }
         drive.setDrivePowers(new PoseVelocity2d(new Vector2d(0, 0), 0));
         PoseStorage.currentPose = drive.pose;
 
+        //close claw
         drive.claw.setPosition(.8);
 
+        //just a delay
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(drive.pose.position, 3*Math.PI/2))
                         .waitSeconds(.125)
                         .build()
         );
 
+        //only if the pick up did not fail indicated by pickUpFail being false
+        //lift up to high chamber scoring position and then hold
         if(!pickUpFail){
             while (((drive.liftLeft.getCurrentPosition() * -1) < 1000) && drive.liftRight.getCurrentPosition() < 1000){
                 drive.liftRight.setPower(Math.max((1000-(drive.liftRight.getCurrentPosition())) * 0.005, 0.1));
@@ -174,9 +210,12 @@ public class RedRedV1 extends LinearOpMode {
             drive.liftLeft.setPower(-.001);
         }
 
+        //set lift arm to scoring position
         drive.liftLeftS.setPosition(0.20);
         drive.liftRightS.setPosition(0.35);
 
+        //move to and score on the high chamber by slaming into it
+        //update the position in poseition storage
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(drive.pose.position, 3*Math.PI/2))
                         .strafeToSplineHeading(new Vector2d(-2,38), Math.PI/2)
@@ -186,6 +225,7 @@ public class RedRedV1 extends LinearOpMode {
         PoseStorage.currentPose = drive.pose;
 
 
+        //lower lift away from the high chamber and then hold
         while (((drive.liftLeft.getCurrentPosition() * -1) > 800) && drive.liftRight.getCurrentPosition() > 800){
             drive.liftRight.setPower(-1);
             drive.liftLeft.setPower(1);
@@ -193,16 +233,21 @@ public class RedRedV1 extends LinearOpMode {
         drive.liftRight.setPower(.001);
         drive.liftLeft.setPower(-.001);
 
+        //open claw
         drive.claw.setPosition(.45);
 
+        //just a delay
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(-2, 30, Math.PI/2))
                         .waitSeconds(.125)
                         .build()
         );
 
+        //close claw
         drive.claw.setPosition(.8);
 
+        //move back into the observation zone
+        //update the position in poseition storage
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(-2, 30, Math.PI/2))
                         .strafeTo(new Vector2d(-2,45))
@@ -211,6 +256,7 @@ public class RedRedV1 extends LinearOpMode {
         );
         PoseStorage.currentPose = drive.pose;
 
+        //lower the lift all the way down
         while (!drive.rightT.isPressed() && !drive.leftT.isPressed()){
             drive.liftRight.setPower(-1);
             drive.liftLeft.setPower(1);
@@ -219,18 +265,24 @@ public class RedRedV1 extends LinearOpMode {
 
         //Specimen 3
 
+        //reset lift encoders
         drive.liftRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         drive.liftLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        //open claw
         drive.claw.setPosition(.45);
 
         drive.liftRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         drive.liftLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        //set lift arm to wall pick up location
         drive.liftLeftS.setPosition(0.25);
         drive.liftRightS.setPosition(0.65);
 
 
+        //check if the rear discance sensors are reading data
+        //if no -> set pickUpFail to true and turn the LEDs white
+        //if yes -> set pickUpFail to false and turn the LEDs red
         if(drive.rBack.getDistance(DistanceUnit.INCH) == 0 || drive.lBack.getDistance(DistanceUnit.INCH) == 0){
             pickUpFail = true;
             drive.lightRight.setPosition(1);
@@ -241,7 +293,10 @@ public class RedRedV1 extends LinearOpMode {
             drive.lightLeft.setPosition(0.3);
         }
 
+        //back up well the 2 rear distance sensor average distance is greater the 6.5
+        //after set movement to zero and update the position in poseition storage
         while((drive.rBack.getDistance(DistanceUnit.INCH) + drive.lBack.getDistance(DistanceUnit.INCH)) / 2 >= 6.5){
+            //do same sensor data check as above
             if(drive.rBack.getDistance(DistanceUnit.INCH) == 0 || drive.lBack.getDistance(DistanceUnit.INCH) == 0){
                 pickUpFail = true;
                 drive.lightRight.setPosition(1);
@@ -251,14 +306,17 @@ public class RedRedV1 extends LinearOpMode {
                 drive.lightRight.setPosition(0.3);
                 drive.lightLeft.setPosition(0.3);
             }
+            //set the movement to 0.25 backwards and update drive position
             drive.setDrivePowers(new PoseVelocity2d(new Vector2d(-0.25, 0), 0));
             drive.updatePoseEstimate();
         }
         drive.setDrivePowers(new PoseVelocity2d(new Vector2d(0, 0), 0));
         PoseStorage.currentPose = drive.pose;
 
+        //close claw
         drive.claw.setPosition(.8);
 
+        //just a delay
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(drive.pose.position, 3*Math.PI/2))
                         .waitSeconds(.125)
@@ -266,6 +324,8 @@ public class RedRedV1 extends LinearOpMode {
         );
 
 
+        //only if the pick up did not fail indicated by pickUpFail being false
+        //lift up to high chamber scoring position and then hold
         if(!pickUpFail){
             while (((drive.liftLeft.getCurrentPosition() * -1) < 1000) && drive.liftRight.getCurrentPosition() < 1000){
                 drive.liftRight.setPower(Math.max((1000-(drive.liftRight.getCurrentPosition())) * 0.005, 0.1));
@@ -275,9 +335,12 @@ public class RedRedV1 extends LinearOpMode {
             drive.liftLeft.setPower(-.001);
         }
 
+        //set lift arm to scoring position
         drive.liftLeftS.setPosition(0.20);
         drive.liftRightS.setPosition(0.35);
 
+        //move to and score on the high chamber by slaming into it
+        //update the position in poseition storage
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(drive.pose.position, 3*Math.PI/2))
                         .strafeToSplineHeading(new Vector2d(-6,38), Math.PI/2)
@@ -287,6 +350,7 @@ public class RedRedV1 extends LinearOpMode {
         PoseStorage.currentPose = drive.pose;
 
 
+        //lower lift away from the high chamber and then hold
         while (((drive.liftLeft.getCurrentPosition() * -1) > 800) && drive.liftRight.getCurrentPosition() > 800){
             drive.liftRight.setPower(-1);
             drive.liftLeft.setPower(1);
@@ -294,19 +358,25 @@ public class RedRedV1 extends LinearOpMode {
         drive.liftRight.setPower(.001);
         drive.liftLeft.setPower(-.001);
 
+        //open claw
         drive.claw.setPosition(.45);
 
+        //just a delay
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(-6, 30, Math.PI/2))
                         .waitSeconds(.125)
                         .build()
         );
 
+        //close claw
         drive.claw.setPosition(.8);
 
+        //extend intake out
         drive.horizontal.setPosition(0.2);
         drive.horizontal2.setPosition(0.4);
 
+        //move to park in observation zone putting the intake in the corner
+        //update the position in poseition storage
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(-6, 30, Math.PI/2))
                         .splineTo(new Vector2d(-48, 56), 3*Math.PI/4)
@@ -314,11 +384,14 @@ public class RedRedV1 extends LinearOpMode {
         );
         PoseStorage.currentPose = drive.pose;
 
+        //lower lift all the way down
         while (!drive.rightT.isPressed() && !drive.leftT.isPressed()){
             drive.liftRight.setPower(-1);
             drive.liftLeft.setPower(1);
         }
 
+        //update the position in poseition storage
         PoseStorage.currentPose = drive.pose;
     }
+
 }
